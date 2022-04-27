3cac6372610be754133cc520136cdb876e5b6292
pingusl <pingusl@yahoo.fr>
2022-04-27 20:56:41 +0200

    main structure definition

--- src/components/main.js
+++ src/components/main.js
@@ -5,6 +5,40 @@ const Main = () => {
   const [switch2, setSwitch2] = useState(false);
   const [switch3, setSwitch3] = useState(false);
 
-  return <main className="main"></main>;
+  return (
+    <main className="main">
+      <div className="col1">
+        <div className="row">
+          <button className="switch-on">
+            <span className="on">ON</span>
+          </button>
+          <button className="switch-off">
+            <span className="off">OFF</span>
+          </button>
+        </div>
+        <div className="row">
+          <button className="switch-on">
+            <span className="on">ON</span>
+          </button>
+          <button className="switch-off">
+            <span className="off">OFF</span>
+          </button>
+        </div>
+        <div className="row">
+          <button className="switch-on">
+            <span className="on">ON</span>
+          </button>
+          <button className="switch-off">
+            <span className="off">OFF</span>
+          </button>
+        </div>
+      </div>
+      <div className="col2">
+        <div className="ligth">
+          <span className="autorisation"></span>
+        </div>
+      </div>
+    </main>
+  );
 };
 export default Main;
